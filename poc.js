f7cd054b1928b57997b7e6f83682a91a94aca9a8
Tony Baker <anthony.s.baker@gmail.com>
2017-04-02 12:39:58 -0400

    new file:   pitchdetector.js modified:   poc.js modified:   test.html

--- poc.js
+++ poc.js
@@ -1,11 +1,9 @@
 
 
 var canvas = $("canvas")[0];
-var currentNote = null;
 
 
 var createNote = function(note) {
-  currentNote = note[0];
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 
   var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
@@ -31,20 +29,44 @@ var createNote = function(note) {
 }
 
 
-// createNote("c/4");
-//
+var availableNotes = ["c/4", "d/4", "e/4", "f/4"];
+var currentNote = null;
+
+var newNote = function() {
+  currentNote = _.sample(availableNotes);
+  createNote(currentNote);
+};
 
 
+var noteDetected = function(stats, pitchDetector) {
+  var normalized = _.replace(_.capitalize(currentNote), '/', '');
+  var lookingForNote = normalized[0];
+  var lookingForOctave = parseInt(normalized[1]);
+
+  var detected = pitchDetector.getNoteString();
+  var detectedNote = detected[0];
+  var detectedOctave = parseInt(detected[1]);
 
 
+  console.log("looking for", lookingForNote, lookingForOctave, "detected", detectedNote, detectedOctave);
+
+  if (lookingForNote == detectedNote && lookingForOctave == detectedOctave + 1) {
+    newNote();
+  }
+};
+
+var detector = new PitchDetector( {
+  context: new AudioContext(),
+  length: 2048,
+  onDetect: noteDetected,
+  start: true,
+  stopAfterDetection: false,
+  normalize: "rms",
+  minRms: 0.15,
+  minCorrelationIncrease: false,
+  minCorrelation: false
+});
 
 
-
-
-
-
-
-
-
-
-
+detector.start();
+newNote();
